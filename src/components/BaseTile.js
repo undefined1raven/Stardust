f6dee4058197b9730f7c0fdefc1050e3a86eca45
raven <zlat.dominic.francisc@gmail.com>
2023-02-10 23:33:14 +0200

    Mobile beta UX testing

--- src/components/BaseTile.js
+++ src/components/BaseTile.js
@@ -12,10 +12,11 @@ const Tile = (props) => {
     }
     return (
         <div title={props.title} onMouseLeave={ml} onMouseEnter={me} style={{ position: 'absolute', width: props.width, height: props.height }} id={props.id}>
-            <svg style={{width: '50%', height: '50%'}} width="88" height="100" viewBox="0 0 88 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+            {/* <svg style={{width: '100%', height: '100%'}} width="88" height="100" viewBox="0 0 88 100" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M86.8013 74.7113L44 99.4226L1.19873 74.7113L1.19873 25.2887L44 0.577354L86.8013 25.2887L86.8013 74.7113Z" fill={props.color} fillOpacity={opacity} stroke={props.color} />
-            </svg>
-
+                </svg> */
+                <div className="waterfallTile" style={{backgroundColor: props.color, opacity: opacity}}></div>
+            }
         </div>
     )
 }
--- src/components/ElementRow.js
+++ src/components/ElementRow.js
@@ -38,9 +38,9 @@ const ElementRow = (props) => {
         }
     }
 
-    const listGen = valArray.map((x, ixx) => <li key={props.element + Math.random()} className="tileContainer"><Tile title={`Dredge Up ID: ${ixx} | ${props.element}`} color={getColor(x)} width="7.2%" className="elementRowTile"></Tile><div style={{color: getTextColor(x)}}></div></li>)//{Math.abs(x).toFixed(2)}
+    const listGen = valArray.map((x, ixx) => <li style={{zIndex: 3}} key={props.element + Math.random()} className="tileContainer"><Tile title={`Dredge Up ID: ${ixx} | ${props.element}`} color={getColor(x)} width="5%" height="40%" className="elementRowTile"></Tile><div style={{color: getTextColor(x)}}></div></li>)//{Math.abs(x).toFixed(2)}
     return (
-        <ul className={`tileRowContainer`} style={{ position: 'absolute', width: "90.364583333%", height: "9.351851852%" }} id={props.id}><li className="tileContainer"><Tile width="7.2%" className="elementRowTile" color="#3500A6"></Tile>{props.element.toString().padEnd(2, '.')}</li>{listGen}</ul>
+        <ul className={`tileRowContainer`} style={{ position: 'absolute', width: "90.364583333%", height: "9.351851852%" }} id={props.id}><li className="tileContainer"><Tile style={{zIndex: 10}} width="6%" height="40%" className="elementRowTile" color="#3500A6"></Tile>{props.element.toString().padEnd(2, '.')}</li>{listGen}</ul>
     )
 }
 
